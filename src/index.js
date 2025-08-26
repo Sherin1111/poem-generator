d33536570f80a162d754e9978b94594617eaab99
Sherin1111 <sherinadam3@gmail.com>
2025-08-26 13:28:43 +0100

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "Beneath the moon's cold, fractured eye, The ancient manor starts to sigh.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
